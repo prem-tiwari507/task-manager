c3a11e2fd816e7c02c5a009240db7d0a47452e05
prem-tiwari507 <premtiwari507@gmail.com>
2025-06-26 14:09:51 +0530

    added angular environments - updated.

--- ui/task-manager-ui/src/environments/environment.prod.ts
+++ ui/task-manager-ui/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'prod_server_url'
+  envName : 'PROD'
 };
--- ui/task-manager-ui/src/environments/environment.ts
+++ ui/task-manager-ui/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8000'
+  envName : 'DEV'
 };
